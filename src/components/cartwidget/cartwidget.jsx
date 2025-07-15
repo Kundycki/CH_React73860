fa58fbf09984fb169943baac46195f6707d29e7f
Kundycki <facundokundycki@gmail.com>
2025-07-15 19:46:58 -0300

    still working

--- src/components/cartwidget/cartwidget.jsx
+++ src/components/cartwidget/cartwidget.jsx
@@ -1,7 +1,13 @@
-export default function Cart() {
+import "./cartwidget.css";
+import { PiShoppingCartFill } from "react-icons/pi";
+
+const Cart = () => {
   return (
-    <div>
-       <span>CARRITO</span>
+    <div className="cart-widget">
+      <PiShoppingCartFill className="cart-icon" />
+      <p className="cart-text"> </p>
     </div>
-  );
+  )
 }
+
+export default Cart;
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,16 +1,33 @@
 import CartWidget from "../cartwidget/cartwidget";
+import { Link } from "react-router-dom";
+import logoreddingtong from "../../assets/logoreddingtong.png";
 import "./navbar.css";
 
-export default function NavBar() {
-  return (
-    <nav className="navbar">
-      <img src="../../assets/logoreddingtong.png" alt="Logo de la tienda" className="logo" />
-      <ul className="nav-links">
-        <li><a href="#">Inicio</a></li>
-        <li><a href="#">Productos</a></li>
-        <li><a href="#">Contacto</a></li>
-      </ul>
-      <CartWidget />
-    </nav>
-  );
+const NavBar = () => {
+    return(
+      <div className="navbar">
+        <Link to="/">
+          <img src={logoreddingtong} alt="" className="logo"/>
+        </Link>
+
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Inicio</Link>
+          </li>
+          <li>
+            <Link to="/category/Ropa">Ropa</Link>
+          </li>
+          <li>
+            <Link to="/category/Zapatillas">Calzado</Link>
+          </li>
+          <li>
+            <Link to="/category/Accesorios">Accesorios</Link>
+          </li>
+        </ul>
+        <CartWidget />
+        </div>
+    )
+
 }
+
+export default NavBar;
