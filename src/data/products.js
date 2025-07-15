0c00bc932651ec1e609f203c4601c573da4d4ad3
Kundycki <facundokundycki@gmail.com>
2025-07-14 22:16:05 -0300

    version 0.1.5

--- src/data/products.js
+++ src/data/products.js
@@ -4,21 +4,80 @@ const products = [
     name: "Nike Air Max 270",
     description: "zapatillas deportivas con tecnología de amortiguación.",
     price: 150,
-    image: "../public/img/airmax.jpg"
+    image: "../public/img/airmax.jpg",
+    category: "Zapatillas"
   },
   {
     id: 2,
     name: "Adidas Ultraboost 22",
     description: "Zapatillas de running con gran comodidad y soporte.",
     price: 147,
-    image: "../public/img/ultraboost.jpg"
+    image: "../public/img/ultraboost.jpg",
+    category: "Zapatillas"
   },
   {
     id: 3,
     name: "Yeezy Boost 700 V2",
     description: "Zapatillas de diseño exclusivo con suela de gran tracción.",
     price: 250,
-    image: "../public/img/yeezy.jpg"
+    image: "../public/img/yeezy.jpg",
+    category: "Zapatillas"
+  },
+  {
+    id: 4,
+    name: "Remera Oversize Blanca",
+    description: "Remera de algodón orgánico, corte oversize unisex.",
+    price: 30,
+    image: "../public/img/remera_oversize_blanca.jpg",
+    category: "Remeras"
+  },
+  {
+    id: 5,
+    name: "Remera Negra Básica",
+    description: "Remera básica negra, cómoda y resistente al uso diario.",
+    price: 25,
+    image: "../public/img/remera_negra.jpg",
+    category: "remeras"
+  },
+  {
+    id: 6,
+    name: "Pantalón Cargo Verde",
+    description: "Pantalón con múltiples bolsillos y ajuste elástico.",
+    price: 60,
+    image: "../public/img/pantalon_cargo_verde.jpg",
+    category: "Pantalones"
+  },
+  {
+    id: 7,
+    name: "Jogger Deportivo Gris",
+    description: "Pantalón jogger con tela suave y elástica.",
+    price: 55,
+    image: "../public/img/jogger_gris.jpg",
+    category: "Pantalones"
+  },
+  {
+    id: 8,
+    name: "Gorra Negra Bordada",
+    description: "Gorra de algodón con logo bordado y ajuste trasero.",
+    price: 20,
+    image: "../public/img/gorra_negra.jpg",
+    category: "Accesorios"
+  },
+  {
+    id: 9,
+    name: "Gafas de sol retro",
+    description: "Anteojos con diseño retro, protección UV400.",
+    price: 35,
+    image: "../public/img/anteojos_retro.jpg",
+    category: "Accesorios"
+  },
+  {
+    id: 10,
+    name: "Riñonera Deportiva",
+    description: "Riñonera compacta para uso urbano o deportivo.",
+    price: 28,
+    image: "../public/img/rinonera.jpg",
+    category: "Accesorios"
   }
 ];
 
