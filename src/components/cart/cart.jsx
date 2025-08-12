ba18410a1d55302f1230954240b364f6d59c78ae
Kundycki <facundokundycki@gmail.com>
2025-08-12 19:05:03 -0300

    cart corregido

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,13 +1,15 @@
 import { useContext } from 'react';
 import { CartContext } from '../../context/cartcontext/cartcontext';
 import './cart.css';
+import { Link } from "react-router-dom";
+import { BsFillTrash3Fill } from "react-icons/bs";
 
 const Cart = () => {
-  const { cart, totalPrice, deleteProductById, deleteCart } = useContext(CartContext);
+  const { cart, totalPrice, removeFromCart, clearCart } = useContext(CartContext);
 
   return (
      <div className="cart" >
-      <h1 className="title-cart">Productos en el carrito</h1>
+      <h1 className="title-cart">Mi Carrito</h1>
       {
         cart.map( (productCart) => (
           <div className="item-cart" key={productCart.id}>
@@ -16,7 +18,7 @@ const Cart = () => {
             <p className="text-item-cart">precio c/u: ${productCart.price}</p>
             <p className="text-item-cart">cantidad: {productCart.quantity}</p>
             <p className="text-item-cart">precio parcial: ${ productCart.price * productCart.quantity } </p>
-            <button className="delete-item-cart" onClick={ () => deleteProductById(productCart.id) } >
+            <button className="delete-item-cart" onClick={ () => removeFromCart(productCart.id) } >
               <BsFillTrash3Fill />
             </button>
           </div>
@@ -26,7 +28,7 @@ const Cart = () => {
       <div className="info-cart">
         <p className="text-info-cart">Precio total: ${totalPrice()}</p>
         <Link className="button-continue-cart" to="/checkout">Continuar con mi compra</Link>
-        <button className="button-delete-cart" onClick={deleteCart} >Vaciar carrito</button>
+        <button className="button-delete-cart" onClick={clearCart} >Vaciar carrito</button>
       </div>
     </div>
   )
